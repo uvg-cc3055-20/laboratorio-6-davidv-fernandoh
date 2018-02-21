d5811a26628cadd9a74b228730b00f9ffe280093
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-02-21 09:11:57 -0600

    Robin

---
+++ Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    private VirtualButtonBehaviour virtualBtn;
+    public Animator birdAnim;
+
+    // Use this for initialization
+    void Start () {
+
+        virtualBtn = GetComponent<VirtualButtonBehaviour>();
+        virtualBtn.RegisterEventHandler(this);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("d");
+        birdAnim.SetTrigger("sing");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+    }
+}
