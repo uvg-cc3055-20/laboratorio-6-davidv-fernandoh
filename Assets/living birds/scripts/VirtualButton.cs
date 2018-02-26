e57c02525c20acb9afa9c507fd62e3b7ddca518f
FernandoHen <11hengsteber@gmail.com>
2018-02-25 19:23:08 -0600

    ,,

--- Assets/living birds/scripts/VirtualButton.cs
+++ Assets/living birds/scripts/VirtualButton.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
-public class VirtualButton : MonoBehaviour {
+public class VirtualButton : MonoBehaviour 
+    
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
 }
